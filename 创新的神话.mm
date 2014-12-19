4cb150d343b71b9155aecd805909e3040ace8331
darksun <lujun9972@sina.com>
2014-12-19 14:20:04 +0800

    init version

---
+++ 创新的神话.mm
@@ -0,0 +1,111 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node text="创新的神话.org" background_color="#00bfff">
+<richcontent TYPE="NOTE"><html><head></head><body><p>--org-mode: WHOLE FILE</p></body></html></richcontent>
+<node text="&#x7075;&#x611f;&#x7684;&#x795e;&#x8bdd;" position="left">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p><b> &#x521b;&#x65b0;&#x662f;&#x9700;&#x8981;&#x5404;&#x79cd;&#x73af;&#x5883;&#x8981;&#x7d20;&#x7684;&#x56e0;&#x7f18;&#x9645;&#x4f1a;&#x7684;&#x914d;&#x5408;&#x7684;<br /></b> &#x8bb8;&#x591a;&#x521b;&#x65b0;&#x8005;,&#x5176;&#x6240;&#x8c13;&#x7684;&#x60f3;&#x6cd5;&#x65e9;&#x5c31;&#x5df2;&#x7ecf;&#x6709;&#x4e86;,&#x4ed6;&#x4eec;&#x6240;&#x4f5c;&#x7684;&#x53ea;&#x662f;&#x628a;&#x8be5;&#x60f3;&#x6cd5;&#x5b9e;&#x73b0;<br /><b> &#x7075;&#x611f;&#x5206;&#x4e3a;&#x4e09;&#x4e2a;&#x90e8;&#x5206;<br />	</b> &#x65e9;&#x671f;<br />	  &#x5728;&#x65e9;&#x671f;,&#x9700;&#x8981;&#x5927;&#x91cf;&#x7684;&#x65f6;&#x95f4;&#x6765;&#x7406;&#x89e3;&#x95ee;&#x9898;&#x5e76;&#x4e14;&#x5168;&#x8eab;&#x6c89;&#x6d78;&#x5728;&#x8fd9;&#x4e00;&#x9886;&#x57df;&#x5f53;&#x4e2d;<br />	<b> &#x9886;&#x609f;<br />	  &#x7ecf;&#x8fc7;&#x4e00;&#x756a;&#x82e6;&#x601d;&#x51a5;&#x60f3;&#x4e4b;&#x540e;,&#x5728;&#x653e;&#x677e;&#x65f6;,&#x6f5c;&#x610f;&#x8bc6;&#x7ee7;&#x7eed;&#x8fd0;&#x884c;,&#x76f4;&#x5230;&#x6709;&#x65b0;&#x7684;&#x60f3;&#x6cd5;&#x5728;&#x6f5c;&#x610f;&#x8bc6;&#x4e2d;&#x6d6e;&#x73b0;<br />	</b> &#x540e;&#x671f;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+<node text="&#x521b;&#x65b0;&#x6709;&#x65b9;&#x6cd5;" position="left">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p><b> &#x5728;&#x67d0;&#x4e2a;&#x7279;&#x5b9a;&#x9886;&#x57df;&#x52aa;&#x529b;&#x5de5;&#x4f5c;<br />	&#x521b;&#x65b0;&#x4e3b;&#x8981;&#x6765;&#x81ea;&#x4e8e;&#x4e3a;&#x4e86;&#x89e3;&#x51b3;&#x4e00;&#x4e2a;&#x660e;&#x786e;&#x7684;&#x95ee;&#x9898;&#x800c;&#x4e13;&#x6ce8;&#x4e8e;&#x67d0;&#x4e2a;&#x9886;&#x57df;&#x91cc;&#x9762;&#x7684;&#x52aa;&#x529b;&#x5de5;&#x4f5c;<br />	&#x521b;&#x65b0;&#x8005;&#x4eec;&#x82b1;&#x65f6;&#x95f4;&#x603b;&#x7ed3;&#x95ee;&#x9898;,&#x5217;&#x4e3e;&#x53ef;&#x80fd;&#x7684;&#x89e3;&#x51b3;&#x65b9;&#x6848;,&#x7136;&#x540e;&#x5f00;&#x59cb;&#x8bd5;&#x9a8c;<br /></b> &#x52aa;&#x529b;&#x5de5;&#x4f5c;&#x52a0;&#x65b9;&#x5411;&#x8c03;&#x6574;<br />	&#x8bb8;&#x591a;&#x521b;&#x65b0;&#x5728;&#x5f00;&#x59cb;&#x65f6;&#x4e5f;&#x8ddf;&#x524d;&#x9762;&#x63d0;&#x5230;&#x8fc7;&#x7684;&#x4e00;&#x6837;,&#x4f46;&#x662f;&#x5728;&#x5de5;&#x4f5c;&#x7684;&#x534a;&#x9014;&#x51fa;&#x73b0;&#x4e86;&#x6ca1;&#x6709;&#x9884;&#x6599;&#x5230;&#x7684;&#x673a;&#x4f1a;&#xff0c;&#x521b;&#x65b0;&#x5c31;&#x8f6c;&#x5411;&#x8fd9;&#x4e2a;&#x65b0;&#x51fa;&#x73b0;&#x7684;&#x673a;&#x4f1a;<br /><b> &#x597d;&#x5947;&#x5fc3;<br /></b> &#x8d22;&#x5bcc;&#x548c;&#x91d1;&#x94b1;&#x7684;&#x9a71;&#x52a8;<br /><b> &#x9700;&#x6c42;&#x7684;&#x9a71;&#x52a8;<br /></b> &#x521b;&#x65b0;&#x5f80;&#x5f80;&#x5305;&#x62ec;&#x4e86;&#x4e0a;&#x8bc9;&#x7684;&#x5404;&#x79cd;&#x7279;&#x70b9;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+<node text="&#x521b;&#x65b0;&#x7684;&#x6311;&#x6218;" position="left">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p><b> &#x5982;&#x4f55;&#x53d1;&#x6398;&#x4e00;&#x4e2a;&#x60f3;&#x6cd5;<br /></b> &#x5982;&#x4f55;&#x5f00;&#x8bf6;&#x53d1;&#x4e00;&#x4e2a;&#x89e3;&#x51b3;&#x65b9;&#x6848;<br /><b> &#x5982;&#x4f55;&#x83b7;&#x5f97;&#x8d5e;&#x52a9;&#x548c;&#x6295;&#x8d44;<br /></b> &#x4f60;&#x7684;&#x65b9;&#x6848;&#x662f;&#x53ef;&#x590d;&#x5236;&#x7684;&#x5417;<br /><b> &#x5982;&#x4f55;&#x627e;&#x5230;&#x4f60;&#x7684;&#x6f5c;&#x5728;&#x5ba2;&#x6237;--&#x521b;&#x65b0;&#x6240;&#x9488;&#x5bf9;&#x7684;&#x76ee;&#x6807;&#x4eba;&#x7fa4;<br /></b> &#x5982;&#x4f55;&#x51fb;&#x8d25;&#x4f60;&#x7684;&#x7ade;&#x4e89;&#x5bf9;&#x624b;<br /><b> &#x4f60;&#x7684;&#x60f3;&#x6cd5;&#x5728;&#x4f60;&#x6240;&#x5904;&#x7684;&#x73af;&#x5883;&#x4e2d;&#x662f;&#x5426;&#x80fd;&#x88ab;&#x4eba;&#x63a5;&#x6536;,&#x662f;&#x5426;&#x80fd;&#x591f;&#x5b9e;&#x73b0;<br /></b> &#x4fdd;&#x6301;&#x5e73;&#x5e38;&#x5fc3;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+<node text="&#x63d0;&#x9ad8;&#x521b;&#x65b0;&#x80dc;&#x7b97;&#x7684;&#x601d;&#x8003;&#x65b9;&#x5f0f;">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p><b> &#x8ba4;&#x8bc6;&#x81ea;&#x6211;<br /></b> &#x6709;&#x6fc0;&#x60c5;&#x4f46;&#x975e;&#x4e00;&#x610f;&#x5b64;&#x884c;<br /><b> &#x5faa;&#x5e8f;&#x6e10;&#x8fdb;<br />	&#x5148;&#x5c1d;&#x8bd5;&#x89e3;&#x51b3;&#x67d0;&#x4e2a;&#x719f;&#x6089;&#x9886;&#x57df;&#x7684;&#x7279;&#x5b9a;&#x95ee;&#x9898;,&#x7136;&#x540e;&#x968f;&#x7740;&#x6210;&#x529f;&#x7684;&#x79ef;&#x7d2f;&#x518d;&#x63d0;&#x9ad8;&#x81ea;&#x5df1;&#x7684;&#x76ee;&#x6807;<br /></b> &#x627f;&#x8ba4;&#x8fd0;&#x6c14;,&#x4e0d;&#x6c89;&#x8ff7;&#x4e8e;&#x6628;&#x65e5;&#x7684;&#x6210;&#x529f;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+<node text="&#x5927;&#x5bb6;&#x559c;&#x7231;&#x65b0;&#x60f3;&#x6cd5;">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p><b> &#x91c7;&#x53d6;&#x4e00;&#x9879;&#x521b;&#x65b0;&#x6240;&#x9700;&#x7684;&#x6539;&#x53d8;&#x8d8a;&#x5927;,&#x5c31;&#x4f1a;&#x5f15;&#x8d77;&#x8d8a;&#x591a;&#x5bf9;&#x6539;&#x53d8;&#x7684;&#x6050;&#x60e7;<br /></b> &#x4eba;&#x4eec;&#x4e0d;&#x53ef;&#x80fd;&#x50cf;&#x4f60;&#x4e00;&#x6837;&#x5bf9;&#x4f60;&#x7684;&#x60f3;&#x6cd5;&#x6709;&#x8fd9;&#x4e48;&#x5927;&#x7684;&#x5174;&#x8da3;<br /><b> &#x521b;&#x65b0;&#x5f88;&#x5c11;&#x56e0;&#x4e3a;&#x5b83;&#x4eec;&#x7684;&#x6210;&#x679c;&#x800c;&#x88ab;&#x62d2;&#x7edd;,&#x5b83;&#x4eec;&#x88ab;&#x62d2;&#x7edd;&#x5f80;&#x5f80;&#x662f;&#x56e0;&#x4e3a;&#x5b83;&#x4eec;&#x5f15;&#x8d77;&#x7684;&#x4eba;&#x4eec;&#x7684;&#x611f;&#x89c9;<br />	</b> &#x5ac9;&#x5992;&#x6211;&#x6ca1;&#x6709;&#x60f3;&#x5230;&#x8fd9;&#x4e2a;&#x60f3;&#x6cd5;<br />	<b> &#x50b2;&#x6162;,&#x8ba4;&#x4e3a;&#x4f60;&#x4e0d;&#x53ef;&#x80fd;&#x60f3;&#x5230;&#x4ec0;&#x4e48;&#x597d;&#x60f3;&#x6cd5;<br />	</b> &#x61d2;&#x60f0;,&#x4e0d;&#x60f3;&#x53bb;&#x601d;&#x8003;&#x8fd9;&#x4e2a;&#x60f3;&#x6cd5;&#x7684;&#x610f;&#x4e49;<br />	<b> &#x8fdd;&#x80cc;&#x4e86;&#x6211;&#x5185;&#x5fc3;&#x6df1;&#x5904;&#x79c9;&#x627f;&#x7684;&#x7406;&#x5ff5;<br />	</b> &#x5b89;&#x5168;&#x611f;,&#x8fd9;&#x4e2a;&#x521b;&#x65b0;&#x53ef;&#x80fd;&#x4f1a;&#x6bc1;&#x4e86;&#x6211;&#x73b0;&#x6709;&#x7684;&#x4e00;&#x5207;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+<node text="&#x5f71;&#x54cd;&#x521b;&#x65b0;&#x63a8;&#x5e7f;&#x901f;&#x5ea6;&#x7684;5&#x4e2a;&#x8981;&#x7d20;">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p><b> &#x76f8;&#x5bf9;&#x4f18;&#x52bf;--&#x65b0;&#x4e8b;&#x7269;&#x8ddf;&#x65e7;&#x4e8b;&#x7269;&#x6bd4;&#x8d77;&#x6765;&#x6709;&#x4ec0;&#x4e48;&#x4ef7;&#x503c;<br /></b> &#x517c;&#x5bb9;&#x6027;--&#x4ece;&#x5f53;&#x524d;&#x7684;&#x4e8b;&#x7269;&#x8f6c;&#x6362;&#x5230;&#x521b;&#x65b0;&#x7684;&#x4e8b;&#x7269;&#x4e0a;&#x9700;&#x8981;&#x591a;&#x5927;&#x7684;&#x6210;&#x672c;<br /><b> &#x590d;&#x6742;&#x5ea6;--&#x5e94;&#x7528;&#x521b;&#x65b0;&#x9700;&#x8981;&#x5b66;&#x4e60;&#x591a;&#x5c11;&#x4e1c;&#x897f;<br /></b> &#x8bd5;&#x7528;&#x6027;--&#x521b;&#x65b0;&#x53ef;&#x4ee5;&#x8bd5;&#x7528;&#x5417;<br />* &#x53ef;&#x89c1;&#x6027;--&#x521b;&#x65b0;&#x7684;&#x6210;&#x679c;&#x6709;&#x591a;&#x4e48;&#x5bb9;&#x6613;&#x770b;&#x89c1;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+<node text="&#x597d;&#x4e3b;&#x610f;&#x5f88;&#x96be;&#x627e;">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p><b> &#x521b;&#x65b0;&#x7684;&#x8868;&#x9762;&#x5f88;&#x5c11;&#x4ee4;&#x4eba;&#x5370;&#x8c61;&#x6df1;&#x523b;,&#x4f46;&#x5173;&#x952e;&#x5728;&#x4e8e;&#x5b83;&#x7684;&#x6f5c;&#x529b;<br /></b> &#x83b7;&#x5f97;&#x4e00;&#x4e2a;&#x597d;&#x4e3b;&#x610f;&#x7684;&#x5173;&#x952e;&#x5728;&#x4e8e;&#x83b7;&#x5f97;&#x5927;&#x91cf;&#x7684;&#x4e3b;&#x610f;,&#x56e0;&#x6b64;&#x968f;&#x65f6;&#x8bb0;&#x5f55;&#x81ea;&#x5df1;&#x7684;&#x60f3;&#x6cd5;&#x662f;&#x5f88;&#x91cd;&#x8981;&#x7684;<br />* </p></body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
